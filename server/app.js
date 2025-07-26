ccd0ebbc15f52df6f02a8fe131c63f55ec64281b
Thien <farenabc123@gmail.com>
2025-07-26 19:22:34 +0700

    Fix: Update all API URLs to use environment variables

--- server/app.js
+++ server/app.js
@@ -36,7 +36,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:3001',
-    'https://manage-customer-in-bank-fe.vercel.app'
+    'https://fe-banking.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
