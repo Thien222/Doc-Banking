d54a63450842c04dce24e6e429961525411d8825
Thien222 <164997085+Thien222@users.noreply.github.com>
2025-07-26 19:07:08 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -60,7 +60,7 @@ const io = new Server(server, {
     origin: [
       'http://localhost:3000',
       'http://localhost:3001',
-      'https://manage-customer-in-bank-fe.vercel.app'
+      'https://fe-bank-frontend.vercel.app'
     ],
     methods: ['GET', 'POST']
   }
